beb572e7c0c00700360fef46e2d4da8a354b94f5
Avrely <miron.dobrovsky.work@gmail.com>
2024-11-16 16:11:33 +0300

    Initial Commit

---
+++ Iconfinder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Iconfinder
+//
+//  Created by Dobrovsky on 16.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ IconfinderTests/IconfinderTests.swift
@@ -0,0 +1,17 @@
+//
+//  IconfinderTests.swift
+//  IconfinderTests
+//
+//  Created by Dobrovsky on 16.11.2024.
+//
+
+import Testing
+@testable import Iconfinder
+
+struct IconfinderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
